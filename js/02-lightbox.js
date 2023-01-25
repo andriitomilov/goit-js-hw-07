0a9dd9a85f380203bb9885e693843551ccca7fca
andriitomilov <andriitomilov@ukr.net>
2023-01-25 12:04:16 +0200

    done task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(array) {
+  return array
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>`
+    )
+    .join("");
+}
+
+
